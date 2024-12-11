1c8425da946081755db39a96312dbf3bc6933d7c
José Gabriel <160707097+JoseGabriel2017@users.noreply.github.com>
2024-12-11 09:22:34 -0300

    Create Atividade_triggers.sql

---
+++ Atividade_triggers.sql
@@ -0,0 +1,37 @@
+--1 - Quando adicionar um defunto deve ser disparado uma trigger que já aloque o defunto em um jazigo;
+DELIMITER $$
+CREATE TRIGGER `after_insert_defunto` 
+AFTER INSERT ON `DEFUNTO`
+FOR EACH ROW
+BEGIN
+
+    DECLARE idJazigoDisponivel INT;
+    SELECT `idJazigo` INTO idJazigoDisponivel
+    FROM `JAZIGO`
+    WHERE `idDefuntoJazigo` = 0
+    LIMIT 1;
+
+    IF idJazigoDisponivel IS NOT NULL THEN
+        UPDATE `JAZIGO`
+        SET `idDefuntoJazigo` = NEW.idDefunto
+        WHERE `idJazigo` = idJazigoDisponivel;
+END$$
+DELIMITER ;
+
+--2 - Quando deletar um defunto deve ser disparado uma triggers para liberar o jazigo oculpado pelo defunto;
+DELIMITER $$
+CREATE TRIGGER `after_delete_defunto` 
+AFTER DELETE ON `DEFUNTO`
+FOR EACH ROW
+BEGIN
+    DECLARE idJazigoOcupado INT;
+    SELECT `idJazigo` INTO idJazigoOcupado
+    FROM `JAZIGO`
+    WHERE `idDefuntoJazigo` = OLD.idDefunto;
+
+    IF idJazigoOcupado IS NOT NULL THEN
+        DELETE FROM `JAZIGO`
+        WHERE `idJazigo` = idJazigoOcupado;
+    END IF;
+END$$
+DELIMITER ;
